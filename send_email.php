d15407f1016826e6811c5c98e8c3b87a10cf65ad
Hunner1985 <127758075+Hunner1985@users.noreply.github.com>
2024-11-17 22:25:30 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,1 +1,20 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $to = 'christian.schmailzl@googlemail.com';
+    $subject = 'Geburtstagsseite aufgerufen';
+    $message = 'Die Geburtstagsseite wurde gerade aufgerufen.';
+    $headers = 'From: christian.schmailzl@googlemail.com' . "\r\n" .
+              /* 'Reply-To: webmaster@example.com' . "\r\n" .*/
+               'X-Mailer: PHP/' . phpversion();
 
+    if (mail($to, $subject, $message, $headers)) {
+        http_response_code(200);
+        echo 'E-Mail gesendet';
+    } else {
+        http_response_code(500);
+        echo 'Fehler beim Senden der E-Mail';
+    }
+} else {
+    http_response_code(405);
+    echo 'Methode nicht erlaubt';
+}
